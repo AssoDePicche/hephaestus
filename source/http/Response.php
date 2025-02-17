32026a09f6fe598233bc2f1da0a8b05b29cec3c1
AssoDePicche <samuelprado730@gmail.com>
2025-02-16 21:10:31 -0300

    feat: cors

--- source/http/Response.php
+++ source/http/Response.php
@@ -11,6 +11,20 @@ namespace Http {
 
         public function send(): void
         {
+            if (isset($_SERVER["HTTP_ORIGIN"])) {
+                header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+
+                header("Access-Control-Allow-Credentials: true");
+            }
+
+            if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"])) {
+                header("Access-Control-Allow-Methods: GET, POST");
+            }
+
+            if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"])) {
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            }
+
             header("Content-Type: application/json");
 
             http_response_code($this->statusCode->value);
