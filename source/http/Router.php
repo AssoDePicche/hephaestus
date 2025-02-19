f343590552ba1023f65d34eeb71288258c5e5e6d
AssoDePicche <samuelprado730@gmail.com>
2025-02-19 02:02:27 -0300

    style: namespace

--- source/http/Router.php
+++ source/http/Router.php
@@ -2,50 +2,50 @@
 
 declare(strict_types=1);
 
-namespace Http {
-    final class Router
+namespace Http;
+
+final class Router
+{
+    private array $routes = [];
+
+    public function __construct(
+        private string $filename
+    ) {
+        $json = file_get_contents($this->filename);
+
+        $this->routes = json_decode($json, true);
+    }
+
+    public function dispatch(Request $request): Response
     {
-        private array $routes = [];
+        $allowed_http_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
 
-        public function __construct(
-            private string $filename
-        ) {
-            $json = file_get_contents($this->filename);
+        foreach ($this->routes as $route => $args) {
+            if ($request->getURI() !== $route) {
+                continue;
+            }
 
-            $this->routes = json_decode($json, true);
-        }
-
-        public function dispatch(Request $request): Response
-        {
-            $allowed_http_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
-
-            foreach ($this->routes as $route => $args) {
-                if ($request->getURI() !== $route) {
+            foreach ($allowed_http_methods as $http_method) {
+                if ($request->getMethod() !== $http_method) {
                     continue;
                 }
 
-                foreach ($allowed_http_methods as $http_method) {
-                    if ($request->getMethod() !== $http_method) {
-                        continue;
-                    }
+                $class = "\\Controller\\" . explode("@", $args[$http_method])[0];
 
-                    $class = "\\Controller\\" . explode("@", $args[$http_method])[0];
+                $callback = explode("@", $args[$http_method])[1];
 
-                    $callback = explode("@", $args[$http_method])[1];
-
-                    if (!class_exists($class) || !method_exists($class, $callback)) {
-                        return new Response([], StatusCode::INTERNAL_SERVER_ERROR);
-                    }
-
-                    $controller = new $class();
-
-                    return call_user_func([$controller, $callback], $request);
+                if (!class_exists($class) || !method_exists($class, $callback)) {
+                    return new Response([], StatusCode::INTERNAL_SERVER_ERROR);
                 }
 
-                return new Response([], StatusCode::METHOD_NOT_ALLOWED);
+                $controller = new $class();
+
+                return call_user_func([$controller, $callback], $request);
             }
 
-            return new Response([], StatusCode::NOT_FOUND);
+            return new Response([], StatusCode::METHOD_NOT_ALLOWED);
         }
+
+        return new Response([], StatusCode::NOT_FOUND);
     }
 }
