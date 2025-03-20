7ceafcb5541df60efbc0a6c6d62ed01967709e82
AssoDePicche <samuelprado730@gmail.com>
2025-03-20 16:33:28 -0300

    feat: UUID implements Stringable interface

--- source/domain/UUID.php
+++ source/domain/UUID.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Domain;
 
-final readonly class UUID
+final readonly class UUID implements \Stringable
 {
     private function __construct(
         private string $value
