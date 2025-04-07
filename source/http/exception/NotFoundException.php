ed5ba5147af9dcdbfdba74cd9ba28cf299affbfe
AssoDePicche <samuelprado730@gmail.com>
2025-04-06 23:54:49 -0300

    feat: NotFoundException.php

---
+++ source/http/exception/NotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Http\Exception;
+
+final class NotFoundException extends \RuntimeException
+{
+    public static function new(string $message = ""): self
+    {
+        return new self($message);
+    }
+}
